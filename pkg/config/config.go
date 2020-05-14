954a186331874b24c61d2b7984a25bc878e48778
IvzhenkoNazar <61440742+IvzhenkoNazar@users.noreply.github.com>
2020-05-14 15:42:03 +0300

    add config package

---
+++ pkg/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+//Load is a function that set configuration
+func Load(configStruct interface{}, configPath string) error {
+	dir, file := filepath.Split(configPath)
+	ext := path.Ext(file)
+	fileExt := strings.TrimPrefix(ext, ".")
+	fileName := strings.TrimSuffix(file, ext)
+
+	viper.SetConfigName(fileName)
+	viper.SetConfigType(fileExt)
+	viper.AddConfigPath(dir)
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	err = viper.Unmarshal(configStruct)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
