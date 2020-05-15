ddf40840d6ed208a6378d8f3ac52f65ba4f4fc64
Vadym <vadym.tyshchenko5@gmail.com>
2020-05-15 18:10:56 +0300

    issue-46, configured RBAC service using pkg/config

--- cmd/rbac/main.go
+++ cmd/rbac/main.go
@@ -1,38 +1,37 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"net/http"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
 
+	"github.com/omc-college/management-system/pkg/config"
 	"github.com/omc-college/management-system/pkg/rbac/api/routers"
 	"github.com/omc-college/management-system/pkg/rbac/repository/postgres"
 )
 
 func main() {
-	// Get DB config
-	userFlag := flag.String("u", "postgres", "user")
-	passwordFlag := flag.String("pw", "superuser", "password")
-	hostFlag := flag.String("h", "localhost", "host")
-	portFlag := flag.String("pt", "5432", "port")
-	databaseFlag := flag.String("db", "roles", "database")
-	sslmodeFlag := flag.String("ssl", "disable", "ssl mode")
+	var serviceConfig Config
+	var err error
+
+	configPath := flag.StringP("config", "c", "./rbac-service-example-config.yaml", "path to service config")
 
 	flag.Parse()
 
-	dbConfig := fmt.Sprintf("user=%s password=%s host=%s port=%s database=%s sslmode=%s",
-		*userFlag, *passwordFlag, *hostFlag, *portFlag, *databaseFlag, *sslmodeFlag)
+	err = config.Load(&serviceConfig, *configPath)
+	if err != nil {
+		logrus.Fatalf("%s", err)
+	}
 
 	// Open DB
-	repository, err := postgres.NewRolesRepository(dbConfig)
+	repository, err := postgres.NewRolesRepository(serviceConfig.RepositoryConfig)
 	if err != nil {
-		log.Fatalf("opening DB error")
+		logrus.Fatalf("opening DB error")
 	}
 
 	defer repository.DB.Close()
 
 	// Start server
-	log.Fatal(http.ListenAndServe(":8000", routers.NewCrudRouter(repository)))
+	logrus.Fatal(http.ListenAndServe(":8000", routers.NewCrudRouter(repository)))
 }
