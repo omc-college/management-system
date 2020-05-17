94d8e1b0b9fd44991d2ebe03a802e40435ad033c
IvzhenkoNazar <61440742+IvzhenkoNazar@users.noreply.github.com>
2020-05-17 20:43:00 +0300

    add pubsub package

--- go.mod
+++ go.mod
@@ -17,6 +17,5 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.7.0
 	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904 // indirect
-	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/yaml.v2 v2.2.8
 )
---
+++ pkg/pubsub/pubsub.go
@@ -0,0 +1,29 @@
+package pubsub
+
+import (
+	stan "nats-io/stan.go"
+)
+
+//GroupClient contains stan.Conn
+type GroupClient struct {
+	stan stan.Conn
+}
+
+//NewQueueGroupClient connected to stan server and return pointer to a GroupClient
+func NewQueueGroupClient(conf Config) (*GroupClient, error) {
+	sc, err := stan.Connect(conf.ClusterID, conf.ClientID, stan.Pings(conf.PingsInterval, conf.MaxUnsuccessfulPings))
+
+	return &GroupClient{
+		stan: sc,
+	}, err
+}
+
+//Publish publishes a message
+func (stanConn *GroupClient) Publish(msg []byte, topicName string) error {
+	err := stanConn.stan.Publish(topicName, msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
