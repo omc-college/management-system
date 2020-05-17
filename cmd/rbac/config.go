1d55076e68abcc11aeb975dedd2fd13e1b0dfef8
IvzhenkoNazar <61440742+IvzhenkoNazar@users.noreply.github.com>
2020-05-17 20:43:00 +0300

    add pubsub package

--- cmd/rbac/config.go
+++ cmd/rbac/config.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/omc-college/management-system/pkg/db"
+	"github.com/omc-college/management-system/pkg/pubsub"
 )
 
 type Config struct {
 	RepositoryConfig db.RepositoryConfig `mapstructure:"repository"`
+	PubSubConfig pubsub.Config `mapstructure:"pubsub"`
 }
