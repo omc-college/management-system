235b8bc4f303f32c3dab08ab8603c0b57c03383a
Matvey <matveyproo1000@gmail.com>
2020-05-22 05:09:03 +0300

    issue-71, implemented user JWT generation
    Co-authored-by: Vadym Tyshchenko <vadym.tyshchenko5@gmail.com>

---
+++ pkg/jwt/models.go
@@ -0,0 +1,11 @@
+package jwt
+
+import "github.com/dgrijalva/jwt-go"
+
+type Claims struct {
+	Sub    string `json:"sub"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Roles  []string `json:"roles"`
+	jwt.StandardClaims
+}
