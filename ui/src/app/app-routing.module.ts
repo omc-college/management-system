95087c241a20f9fc7c45244c7d5b1dde5d379b27
GrandShow <Vashuk2001@gmail.com>
2020-05-30 16:07:50 +0300

    issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,24 +1,37 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {Routes, RouterModule, CanActivate} from '@angular/router';
 
 import {TimetableComponent} from './timetable/timetable.component';
 import {SignInComponent} from './sign-in/sign-in/sign-in.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {ErrorPageComponent} from './error-page/error-page.component';
 
+<<<<<<< HEAD
 import {AdminComponent} from './admin/admin.component';
 
+=======
+class FutureGuard implements CanActivate {
+  canActivate() {
+    return true;
+  }
+}
+>>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
 const routes: Routes = [
   {path: '', redirectTo: '/landing', pathMatch: 'full'},
   {path: 'landing', component: LandingPageComponent, pathMatch: 'full'},
   {path: 'sign-in', component: SignInComponent, pathMatch: 'full'},
+<<<<<<< HEAD
   {path: 'timetable', component: TimetableComponent, pathMatch: 'full'},
   {path: 'admin', component: AdminComponent, pathMatch: 'full'},
+=======
+  {path: 'timetable', canActivate: [FutureGuard], component: TimetableComponent, pathMatch: 'full'},
+>>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
   {path: '**', component: ErrorPageComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [FutureGuard],
 })
 export class AppRoutingModule {}
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -10,8 +10,28 @@ import {LandingPageModule} from './landing-page/landing-page.module';
 import {ErrorPageModule} from './error-page/error-page.module';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+<<<<<<< HEAD
 
 import {AdminModule} from './admin/admin.module';
+
+import {AppComponent} from './app.component';
+=======
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatExpansionModule} from '@angular/material/expansion';
+>>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
 
 import {AppComponent} from './app.component';
 
@@ -23,8 +43,11 @@ import {AppComponent} from './app.component';
     LandingPageModule,
     ErrorPageModule,
 
+<<<<<<< HEAD
     AdminModule,
 
+=======
+>>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
