54c9abb5f6359ec56b6e072b41382952b5fb723e
GrandShow <Vashuk2001@gmail.com>
2020-05-30 16:07:50 +0300

    issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import {TimetableComponent} from './timetable/timetable.component';
 import {SignInComponent} from './sign-in/sign-in/sign-in.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {ErrorPageComponent} from './error-page/error-page.component';
+<<<<<<< HEAD
 import {AdminComponent} from './admin/admin.component';
+=======
+
+>>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
 class FutureGuard implements CanActivate {
   canActivate() {
     return true;
@@ -16,7 +20,10 @@ const routes: Routes = [
   {path: 'landing', component: LandingPageComponent, pathMatch: 'full'},
   {path: 'sign-in', component: SignInComponent, pathMatch: 'full'},
   {path: 'timetable', canActivate: [FutureGuard], component: TimetableComponent, pathMatch: 'full'},
+<<<<<<< HEAD
   {path: 'admin', canActivate: [FutureGuard], component: AdminComponent, pathMatch: 'full'},
+=======
+>>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
   {path: '**', component: ErrorPageComponent},
 ];
 
