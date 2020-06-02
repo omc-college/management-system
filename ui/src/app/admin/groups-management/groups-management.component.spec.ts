6b552cb3d3c9029aeab4f7390560a6ad716da3d8
GrandShow <Vashuk2001@gmail.com>
2020-06-02 15:52:47 +0300

    cherry-picked

---
+++ ui/src/app/admin/groups-management/groups-management.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GroupsManagementComponent } from './groups-management.component';
+
+describe('GroupsManagementComponent', () => {
+  let component: GroupsManagementComponent;
+  let fixture: ComponentFixture<GroupsManagementComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GroupsManagementComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GroupsManagementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
