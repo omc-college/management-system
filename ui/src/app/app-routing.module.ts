02f5242465e3e7204b6d4e043db03de6ba630d50
GrandShow <Vashuk2001@gmail.com>
2020-06-02 15:52:47 +0300

    issue-99, admin panel added, created methods for working with rbac

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,35 +1,24 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule, CanActivate} from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
 
 import {TimetableComponent} from './timetable/timetable.component';
 import {SignInComponent} from './sign-in/sign-in/sign-in.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {ErrorPageComponent} from './error-page/error-page.component';
-<<<<<<< HEAD
-import {AdminComponent} from './admin/admin.component';
-=======
 
->>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
-class FutureGuard implements CanActivate {
-  canActivate() {
-    return true;
-  }
-}
+import {AdminComponent} from './admin/admin.component';
+
 const routes: Routes = [
   {path: '', redirectTo: '/landing', pathMatch: 'full'},
   {path: 'landing', component: LandingPageComponent, pathMatch: 'full'},
   {path: 'sign-in', component: SignInComponent, pathMatch: 'full'},
-  {path: 'timetable', canActivate: [FutureGuard], component: TimetableComponent, pathMatch: 'full'},
-<<<<<<< HEAD
-  {path: 'admin', canActivate: [FutureGuard], component: AdminComponent, pathMatch: 'full'},
-=======
->>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
+  {path: 'timetable', component: TimetableComponent, pathMatch: 'full'},
+  {path: 'admin', component: AdminComponent, pathMatch: 'full'},
   {path: '**', component: ErrorPageComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [FutureGuard],
 })
 export class AppRoutingModule {}
