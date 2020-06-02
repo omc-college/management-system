01020be92f55fd403ce4d6a1daf8f454bcbffd14
GrandShow <Vashuk2001@gmail.com>
2020-06-02 15:52:47 +0300

    issue-99, admin panel added, created methods for working with rbac

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -8,30 +8,9 @@ import {TimetableModule} from './timetable/timetable.module';
 import {SignInModule} from './sign-in/sign-in.module';
 import {LandingPageModule} from './landing-page/landing-page.module';
 import {ErrorPageModule} from './error-page/error-page.module';
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-
 import {AdminModule} from './admin/admin.module';
 
-import {AppComponent} from './app.component';
-=======
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatButtonModule} from '@angular/material/button';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatIconModule} from '@angular/material/icon';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatCardModule} from '@angular/material/card';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatExpansionModule} from '@angular/material/expansion';
->>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 
@@ -42,12 +21,10 @@ import {AppComponent} from './app.component';
     TimetableModule,
     LandingPageModule,
     ErrorPageModule,
-
-<<<<<<< HEAD
     AdminModule,
 
-=======
->>>>>>> issue-96, ui structure of component rewrited, created sign-in/up, error and landing pages
+    AdminModule,
+
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
