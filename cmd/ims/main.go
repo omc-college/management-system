f7563215cbc5258e96a1a4c07089af3912cfbd85
Krimyllex <61523411+Krimyllex@users.noreply.github.com>
2020-06-20 23:41:21 +0300

    `main.go` for SignUp
    `main.go`: created
    `router.go`: changed router's url

--- cmd/ims/main.go
+++ cmd/ims/main.go
@@ -1,8 +1,50 @@
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+	"net/http"
+
+	"github.com/omc-college/management-system/pkg/ims/api/routers"
+	"github.com/omc-college/management-system/pkg/ims/service"
+)
+
+type connection struct {
+	user string;
+	password string;
+	host string;
+	port string;
+	database string;
+	sslmode string;
+}
 
 func main() {
-	fmt.Println("I'm identity management service")
+	var err error
+	var conn connection
+
+	// Mock data
+	conn.user = "postgres";
+	conn.password = "krimyllexadmin";
+	conn.host = "localhost";
+	conn.port = "5432";
+	conn.database = "ims";
+	conn.sslmode = "disable";
+
+	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s database=%s sslmode=%s",
+		conn.user, conn.password, conn.host, conn.port, conn.database, conn.sslmode)
+
+	// Open DB
+	db, err := sqlx.Connect("pgx", dsn)
+	if err != nil {
+		logrus.Fatalf("cannot connect to DB: %s", err.Error())
+	}
+
+	defer db.Close()
+
+	signupService := service.NewSignUpService(db)
+
+	// Start server
+	logrus.Fatal(http.ListenAndServe(":8080", routers.NewSignUpRouter(signupService)))
+
 }
--- pkg/ims/api/routers/router.go
+++ pkg/ims/api/routers/router.go
@@ -16,7 +16,7 @@ func NewSignUpRouter (service *service.SignUpService) *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/signup", signUpHandler.SignUp).Methods(http.MethodPost)
+	router.HandleFunc("/sign-up", signUpHandler.SignUp).Methods(http.MethodPost)
 	router.HandleFunc("/email/available/{email}", signUpHandler.EmailAvailable).Methods(http.MethodGet)
 	router.HandleFunc("/users/emailVerificationToken/verify/{verification_token}", signUpHandler.CheckEmailVerificationToken).Methods(http.MethodGet)
 
