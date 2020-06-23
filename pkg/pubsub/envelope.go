5f0849160d6313049659aee47f8d893d42773d57
IvzhenkoNazar <61440742+IvzhenkoNazar@users.noreply.github.com>
2020-06-23 23:30:19 +0300

    issue-170, add interface for Envelope

--- pkg/pubsub/envelope.go
+++ pkg/pubsub/envelope.go
@@ -6,27 +6,34 @@ import (
 	"github.com/nats-io/stan.go"
 )
 
-type Envelope struct {
-	Type string
-	Operation string
-	Payload json.RawMessage
+type Envelope interface {
+	EntityType() string
+	Operation() string
+	Payload() json.RawMessage
+	Ack() error
+}
+
+type envelope struct {
+	entityType string
+	operation string
+	payload json.RawMessage
 	msg *stan.Msg
 }
 
-func NewEnvelope(payload interface{}, operation string, entityType string) (*Envelope, error) {
+func NewEnvelope(payload interface{}, operation string, entityType string) (*envelope, error) {
 	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Envelope{
-		Type:      entityType,
-		Operation: operation,
-		Payload:   payloadBytes,
+	return &envelope{
+		entityType: entityType,
+		operation: operation,
+		payload:   payloadBytes,
 	}, err
 }
 
-func (e *Envelope)Ack() error {
+func (e *envelope)Ack() error {
 	err := e.msg.Ack()
 	if err != nil {
 		return err
@@ -35,3 +42,14 @@ func (e *Envelope)Ack() error {
 	return nil
 }
 
+func (e *envelope)EntityType() string {
+	return e.entityType
+}
+
+func (e *envelope)Operation() string {
+	return e.operation
+}
+
+func (e *envelope)Payload() json.RawMessage {
+	return e.payload
+}
